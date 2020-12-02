aae56ada98008f3df67c18a3e78ed54bc5d7073f
Alex Mirkhaydarov <alex.mirkhaydarov@gmail.com>
2020-12-02 21:11:39 +0000

    script to standup the elk stack
    Signed-off-by: Alex Mirkhaydarov <alex.mirkhaydarov@gmail.com>

---
+++ kubernetes/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+kubectl create -f "${PWD}/elastic/kubernetes/manifests/*yml"
+
+# Install ElasticSearch
+helm install elasticsearch elastic/elasticsearch -n elastic -f "${PWD}/helm/aarch64/elasticsearch/values.yml"
+
+sleep 30
+
+# Install Logtash
+helm install logstash elastic/logstash -n elastic -f "${PWD}/helm/aarch64/logstash/values.yml"
+
+sleep 30
+
+# Install Kibana
+helm install kibana elastic/kibana -n elastic -f "${PWD}/helm/aarch64/kibana/values.yml"
